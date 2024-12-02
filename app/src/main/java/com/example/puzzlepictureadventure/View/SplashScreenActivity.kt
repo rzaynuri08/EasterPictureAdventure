135f91e7651b5f3335ac9be5ba5b6a1fd8b19294
rzaynuri08 <rzaynuri08@gmail.com>
2024-12-02 15:41:53 +0700

    add Splash Screen Activity

--- app/src/main/java/com/example/puzzlepictureadventure/View/SplashScreenActivity.kt
+++ app/src/main/java/com/example/puzzlepictureadventure/View/SplashScreenActivity.kt
@@ -1,21 +1,25 @@
 package com.example.puzzlepictureadventure.View
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.puzzlepictureadventure.MainActivity
 import com.example.puzzlepictureadventure.R
 
-class SplashScreenActivity : AppCompatActivity() {
+class   SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        // Tetap menggunakan layout XML untuk splash screen
         setContentView(R.layout.activity_splash_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Simulasi loading atau delay (3 detik)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Menutup SplashScreenActivity
+        }, 3000)
     }
 }
